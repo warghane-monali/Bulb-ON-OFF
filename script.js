702e3b2e32d46c47f509617e633ccfca1d3fbbcd
warghane-monali <monaliwarghane97@gmail.com>
2022-11-01 23:35:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const bulbOn = () =>{
+ let bulbon = document.querySelector('.bulboff');
+ bulbon.setAttribute('src', 'images/bulb-on.png');
+ 
+}
+const bulbOff = () =>{
+    let bulboff = document.querySelector('.bulboff');
+    bulboff.setAttribute('src', 'images/bulb-off.png');
+    
+   }
